cde3dc4507082f4c41bd8454f8c3ab5cdbafe2ef
magnus udbjorg <magnus@udbjorg.net>
2017-02-13 21:58:12 +0100

    updates to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
+echo "Button Start"
 cd button 
-npm install
+#npm install
 cp button.service /etc/systemd/system
 systemctl daemon-reload
 systemctl start button.service
 systemctl enable button.service
 cd ..
+echo "Button stop"
 
+echo "Quote Start"
 cd quote-site 
-npm install
+#npm install
 cp quoter.service /etc/systemd/system
 systemctl daemon-reload
 systemctl start quoter.service
 systemctl enable quoter.service
 cd ..
+echo "Quote stop"
 
+echo "Speaker Start"
 cd speaker
-npm install
+#npm install
 cp speaker.service /etc/systemd/system
 systemctl daemon-reload
 systemctl start speaker.service
 systemctl enable speaker.service
 cd ..
+echo "Speaker"
 
 cp redis.service /etc/systemd/system
 systemctl daemon-reload
